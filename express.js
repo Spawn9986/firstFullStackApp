150ef3531d982329b208878ca2d41fcff65789d0
Spawn9986 <scouch9986@gmail.com>
2023-03-08 16:19:14 -0600

    Server created and listening

--- express.js
+++ express.js
@@ -4,4 +4,14 @@ const express = require('express');
 // =========== Global Variables ===========
 const app = express();
 const PORT = 3056
-const { pool } = new POOL
+
+// start developing our routing
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+// Get the server to start listening for requests and have some error handeling
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
