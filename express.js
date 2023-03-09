84840ab773b1e6fda97ac3776d60dbd5c0f5b841
Spawn9986 <scouch9986@gmail.com>
2023-03-08 20:37:46 -0600

    added docker & yaml file and got server connected to the database - working on Front end now

--- express.js
+++ express.js
@@ -1,13 +1,37 @@
 // =========== Require These ==============
 const express = require('express');
+const { Pool } = require('pg');
 
 // =========== Global Variables ===========
 const app = express();
-const PORT = 3056
+const PORT = process.env.PORT || 8000;
+ 
+const pool = new Pool({
+  host: 'postgres-db',
+  user: 'postgres',
+  password: 'password',
+  port: '5432',
+  database: 'people-filter',
+})
+
+//app.use(express.json());
 
 // start developing our routing
+// get all from tables
+
+// test request to verify this file is working
+app.get("/test", (req, res, next) => {
+  res.send('Programming is awesome! This page works!');
+})
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+    pool.query ('SELECT * FROM people', (err, result) => {
+      if (err) {
+        return res.send(err);
+      }
+      let rows = result.rows
+      res.send(rows);
+    })
   })
 
 // Get the server to start listening for requests and have some error handeling
